8759671da7a3c8cfce267f93200cbc572b07a950
William Jagels <will.jagels@gmail.com>
2015-09-27 11:12:08 -0400

    OOOOOER

--- util/buzzfeed.js
+++ util/buzzfeed.js
@@ -19,11 +19,11 @@ module.exports.buzz = function(args, callback) {
 }
 
 module.exports.feeds = function(args,callback) {
-    var el = ["1", "2"," 3", "4","5","6","7","8","9","10"];
+    var el = ["1", "2"," 3", "4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
     var json = [];
     async.forEachOf(el, function(value, key, callback2) {
         module.exports.get('feeds/' + args.feed + "?p=" + value, args, function(error, body) {
-            console.log(json);
+            // console.log(json);
             json = json.concat(JSON.parse(body).flow);
             callback2();
         });
