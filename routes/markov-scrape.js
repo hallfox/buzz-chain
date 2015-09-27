c42c57b6ac58349b5b65b14f55579806f1c60504
William Jagels <will.jagels@gmail.com>
2015-09-26 23:01:03 -0400

    Async stuff

--- routes/markov-scrape.js
+++ routes/markov-scrape.js
@@ -1,12 +1,13 @@
 var striptags = require('striptags');
 var buzzfeed = require('../util/buzzfeed');
 var mongodb = require('mongodb').MongoClient;
+var async = require('async');
 
 
 mongodb.connect('mongodb://localhost:27017/buzz', function(err, db) {
     var scraper = function(err, body) {
         var feed = body;
-        feed.flow.forEach(function(article) {
+        async.each(feed.flow, function(article, callback) {
             var buzzId = article.content.buzz_id;
             buzzfeed.buzz({'buzz_id': buzzId}, function(error, body) {
                 var buzz = body.buzz;
@@ -26,9 +27,13 @@ mongodb.connect('mongodb://localhost:27017/buzz', function(err, db) {
                     }
                 });
                 // Insert into db
-                collection.insert(interestingContent);
+                collection.insert(interestingContent, callback);
                 // console.log(interestingContent);
             });
+        }, function(err) {
+            if(err)
+                console.log(err);
+            db.close()
         });
     }
 
