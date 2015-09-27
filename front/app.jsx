0c76e6c2b14b8cc20e6ee49ba1af1171c71c80fc
Tim Hung <thung1@binghamton.edu>
2015-09-27 04:37:36 +0100

    Add new sample pepes

--- front/app.jsx
+++ front/app.jsx
@@ -1,9 +1,9 @@
 var BUZZDATA = [
   {title: 'Pepe didn\'t eat dinner.', media: 'https://i.imgur.com/19lNyQ8h.jpg'},
-  {title: 'Pepe didn\'t get good grades.', media: 'https://i.imgur.com/19lNyQ8h.jpg'},
-  {title: 'Pepe didn\'t make friends.', media: 'https://i.imgur.com/19lNyQ8h.jpg'},
-  {title: 'Pepe didn\'t make a difference.', media: 'https://i.imgur.com/19lNyQ8h.jpg'},
-  {title: 'Pepe didn\'t matter.', media: 'https://i.imgur.com/19lNyQ8h.jpg'}
+  {title: 'Pepe didn\'t get good grades.', media: 'http://i.imgur.com/ebMsSFY.png'},
+  {title: 'Pepe didn\'t make friends.', media: 'http://i.imgur.com/dCT18GU.jpg'},
+  {title: 'Pepe didn\'t make a difference.', media: 'http://i.imgur.com/fSSh7df.png'},
+  {title: 'Pepe didn\'t matter.', media: 'http://i.imgur.com/Mo3fHAa.jpg'}
 ];
 
 var Article = require('./components/article.jsx');
