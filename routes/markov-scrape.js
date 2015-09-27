d800f4b54b514093ba5cbc8cfcf6381973a240ba
William Jagels <will.jagels@gmail.com>
2015-09-26 22:40:54 -0400

    Mongo fix

--- routes/markov-scrape.js
+++ routes/markov-scrape.js
@@ -3,35 +3,35 @@ var buzzfeed = require('../util/buzzfeed');
 var mongodb = require('mongodb').MongoClient;
 
 
-var scraper = function(err, body) {
-    var feed = body;
-    feed.flow.forEach(function(article) {
-        var buzzId = article.content.buzz_id;
-        buzzfeed.buzz({'buzz_id': buzzId}, function(error, body) {
-            var buzz = body.buzz;
-            var interestingContent = {
-                buzz_id: buzzId,
-                title: article.content.feed.title,
-                buzz_description: buzz.description,
-                headers: [],
-                description: []
-            };
-            buzz.sub_buzzes.forEach(function(d) {
-                if (d.header) {
-                    interestingContent.headers.push(striptags(d.header));
-                }
-                if (d.description) {
-                    interestingContent.description.push(striptags(d.description));
-                }
-            });
-            // Insert into db
-            collection.insert(interestingContent);
-            // console.log(interestingContent);
-        });
-    });
-}
-
 mongodb.connect('mongodb://localhost:27017/buzz', function(err, db) {
+    var scraper = function(err, body) {
+        var feed = body;
+        feed.flow.forEach(function(article) {
+            var buzzId = article.content.buzz_id;
+            buzzfeed.buzz({'buzz_id': buzzId}, function(error, body) {
+                var buzz = body.buzz;
+                var interestingContent = {
+                    buzz_id: buzzId,
+                    title: article.content.feed.title,
+                    buzz_description: buzz.description,
+                    headers: [],
+                    description: []
+                };
+                buzz.sub_buzzes.forEach(function(d) {
+                    if (d.header) {
+                        interestingContent.headers.push(striptags(d.header));
+                    }
+                    if (d.description) {
+                        interestingContent.description.push(striptags(d.description));
+                    }
+                });
+                // Insert into db
+                collection.insert(interestingContent);
+                // console.log(interestingContent);
+            });
+        });
+    }
+
     var collection = db.collection('stories');
     buzzfeed.feeds({'feed': 'lol'}, scraper);
 });
