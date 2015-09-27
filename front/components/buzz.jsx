d12b43213e3b2ea85179a91b521cc5db8117fede
Idealistvishal <rohra.vj@gmail.com>
2015-09-27 06:27:46 -0400

    retry

--- front/components/buzz.jsx
+++ front/components/buzz.jsx
@@ -4,7 +4,7 @@ var Buzz = React.createClass({
         return (
             <div className="buzz">
                 <div className="buzz-title">{this.props.index}. {this.props.data.title}</div>
-                <className="images" img src={this.props.data.media} />
+                <div className="images"> <img src={this.props.data.media} /></div>
             </div>
         );
     }
