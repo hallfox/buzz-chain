905cd59a06dc7bd6c954eae0504e5f9f5d33cb1a
Tim Hung <thung1@binghamton.edu>
2015-09-27 07:00:48 +0100

    spacing

--- front/components/buzz.jsx
+++ front/components/buzz.jsx
@@ -2,7 +2,7 @@ var Buzz = React.createClass({
     render() {
         console.log("In buzz", this.props);
         return (
-            <div>
+            <div className="buzz">
                 <div className="buzz-title">{this.props.data.title}</div>
                 <img src={this.props.data.media} />
             </div>
