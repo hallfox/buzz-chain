8fe6d564c86832ebb3bf8e840a36ec01e7bd30f7
Idealistvishal <rohra.vj@gmail.com>
2015-09-27 05:16:04 -0400

    Changes in logo

--- front/components/article.jsx
+++ front/components/article.jsx
@@ -10,7 +10,7 @@ var Article = React.createClass({
         return (
             <div className="container">
                 <div>
-                    <img src={'http://i62.tinypic.com/33vkenc.png'} alt="logo" height="225" width="400"/>
+                    <img src={'http://i60.tinypic.com/aorl7s.png'} alt="logo" height="59" width="379"/>
                     <h1 className="article-title">9 Things the Pope Hates About Node.js</h1>
                     <div className="author">
                         <div className="author-image"></div>
