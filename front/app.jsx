3167043302f0394460f5cc6fed84b97b40ad9ab4
Tim Hung <thung1@binghamton.edu>
2015-09-27 16:14:23 +0100

    Added api call

--- front/app.jsx
+++ front/app.jsx
@@ -1,22 +1,26 @@
-var data = {
-	title: "Something buzz stupid",
-	author: {
-		name: "Richard Stallman",
-		image: "RS.png"
-	},
-	buzzes: [
-		{
-			title: 'Pepe didnt eat dinner',
-			images: [
-				"http://i.imgur.com/Mo3fHAa.jpg"
-			],
-			desc: "Some stupid image"
-		}
-	]
-};
+// var data = {
+// 	title: "Something buzz stupid",
+// 	author: {
+// 		name: "Richard Stallman",
+// 		image: "RS.png"
+// 	},
+// 	buzzes: [
+// 		{
+// 			title: 'Pepe didnt eat dinner',
+// 			images: [
+// 				"http://i.imgur.com/Mo3fHAa.jpg"
+// 			],
+// 			desc: "Some stupid image"
+// 		}
+// 	]
+// };
 
-var Article = require('./components/article.jsx');
+$.get( "http://buzz-chain.jagels.io/api/mark", function(data) {
+	var Article = require('./components/article.jsx');
 
-$(document).ready(function() {
-    React.render(<Article buzzData={data} />, document.getElementById('buzz-chain'));
+	$(document).ready(function() {
+	    React.render(<Article buzzData={data} />, document.getElementById('buzz-chain'));
+	});
+
 });
+
--- front/components/article.jsx
+++ front/components/article.jsx
@@ -11,10 +11,10 @@ var Article = React.createClass({
             <div className="container">
                 <div>
                     <img src={'http://i62.tinypic.com/2jcxcnp.png'} alt="logo" height="69" width="381"/>
-                    <h1 className="article-title">5 Things the Pope Hates About Node.js</h1>
+                    <h1 className="article-title">{this.props.buzzData.title}</h1>
                     <div className="author">
-                        <div className="author-image"></div>
-                        <div className="author-name">Richard Stallman</div>
+                        <img className="author-image" src={this.props.buzzData.author.image}></img>
+                        <div className="author-name">{this.props.buzzData.author.name}</div>
                         <p>BuzzFeed Staff</p>
                     </div>
                     <SocialMedia />
