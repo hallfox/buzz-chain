1addd7391dda5e4fc68fa605655ccbdbd7edab16
Idealistvishal <rohra.vj@gmail.com>
2015-09-27 09:20:57 -0400

    media query try

--- front/components/buzz.jsx
+++ front/components/buzz.jsx
@@ -4,10 +4,13 @@ var Buzz = React.createClass({
         return (
             <div className="buzz">
                 <div className="buzz-title">{this.props.index}. {this.props.data.title}</div>
-                <div className="imagess"><img src={this.props.data.media} /></div>
+                <div className="imagess"><img src={this.props.data.media}/></div>
             </div>
         );
     }
 });
 
 module.exports = Buzz;
+
+@media (min-width:500px){imagess{width:"620px"}}
+@media (max-width:499px){imagess{width:100%}}
