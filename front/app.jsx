83de5355803560b1aad474043db13abad4972e74
Tim Hung <thung1@binghamton.edu>
2015-09-27 14:57:21 +0100

    Prepare for json api data

--- front/app.jsx
+++ front/app.jsx
@@ -1,4 +1,5 @@
 var BUZZDATA = [
+
   {title: 'Pepe didn\'t eat dinner.', media: 'https://i.imgur.com/19lNyQ8h.jpg'},
   {title: 'Pepe didn\'t get good grades.', media: 'http://i.imgur.com/ebMsSFY.png'},
   {title: 'Pepe didn\'t make friends.', media: 'http://i.imgur.com/dCT18GU.jpg'},
@@ -6,10 +7,28 @@ var BUZZDATA = [
   {title: 'Pepe didn\'t matter.', media: 'http://i.imgur.com/Mo3fHAa.jpg'}
 ];
 
+var data = {
+	title: "Something buzz stupid",
+	author: {
+		name: "Pine",
+		image: "RS.png"
+	},
+	buzzes: [
+		{
+			title: 'Pepe didnt eat dinner',
+			images: [
+				"http://i.imgur.com/Mo3fHAa.jpg"
+			],
+			desc: "Some stupid image"
+		}
+	]
+
+}
+
 var Article = require('./components/article.jsx');
 
 $(document).ready(function() {
-  React.render(<Article buzzData={BUZZDATA} />, document.getElementById('buzz-chain'));
+  React.render(<Article buzzData={data} />, document.getElementById('buzz-chain'));
 });
 
 /*
--- front/components/buzz.jsx
+++ front/components/buzz.jsx
@@ -1,10 +1,13 @@
+
+
 var Buzz = React.createClass({
     render() {
+    	
         console.log("In buzz", this.props);
         return (
             <div className="buzz">
                 <div className="buzz-title">{this.props.index}. {this.props.data.title}</div>
-                <div className="imagess"><img src={this.props.data.media}/></div>
+                <div className="imagess"><img src={this.props.data.image.map}/></div>
             </div>
         );
     }
